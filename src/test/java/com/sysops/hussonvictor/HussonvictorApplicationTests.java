d976fb333478034082211a8108c2af53a7e71316
VICTOR HUSSON <victor.husson1@epsi.fr>
2020-03-02 17:27:51 +0100

    first commit

---
+++ src/test/java/com/sysops/hussonvictor/HussonvictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysops.hussonvictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HussonvictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
